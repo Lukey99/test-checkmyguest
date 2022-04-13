9b7b0a4d072b8bcf65dcde01f9160e622084d28a
Kevin Nguyen <kenguyen@et.esiea.fr>
2022-04-13 22:43:57 +0200

    html et scss

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Main msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Main from './components/main.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Main,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/main.vue
@@ -0,0 +1,13 @@
+<template src="./main.html"></template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Main extends Vue {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" src="./main.scss"></style>
